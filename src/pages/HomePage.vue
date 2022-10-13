28b7a9d5b76889db6b9cb7a6cee3f420c77d6e42
yohannesakd <yoniassefayoni@gmail.com>
2022-10-13 14:49:28 +0300

    edit homepage text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
   <section class="grid place-items-center h-80">
-    <h2>Welcome to the Aimlab Benchmarks Viewer</h2>
+    <h2>Welcome to the Aimtrainer Stats Viewer</h2>
   </section>
 </template>
