fe1be972413f190651f27aa8231a1716b3b7c564
yohannesakd <yoniassefayoni@gmail.com>
2022-10-14 12:27:33 +0300

    test ra bench

---
+++ src/helpers/revosectData.js
@@ -0,0 +1,20 @@
+export const categories = [
+  "Static",
+  "Dynamic",
+  "Precise",
+  "Reactive",
+  "Flick",
+  "Track",
+];
+
+let bench = {
+  id: "CsLevel.VT Empyrean.VT mpXY .R7WQSA",
+  weapon: "Custom_VTnoADS",
+  name: "VT Angleshot Advanced",
+  category: "Clicking",
+  subCategory: "Dynamic",
+  categoryID: 1,
+  scenarioID: 1,
+  scores: [680, 850, 1020, 1100, 1140],
+  steamId: 2765722547,
+};
